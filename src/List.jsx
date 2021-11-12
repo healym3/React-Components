b49e5cfddaa65952d34b39f4cec29f6b5968b126
healym3 <79485702+healym3@users.noreply.github.com>
2021-11-12 11:30:33 -0600

    Break out into components

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Pizza</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
